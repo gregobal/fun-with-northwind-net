19584669afb55bdaf38d053106c72d51ac855744
Gregory Balabanov <810411@mail.ru>
2023-06-23 08:41:23 -0700

    feat: db context and two models

---
+++ NorthwindEFCore/NorthwindDb.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NorthwindEFCore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NorthwindEFCore;
+
+public class NorthwindDb: DbContext
+{
+    public DbSet<Category>? Categories { get; set; }
+    public DbSet<Product>? Products { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        var connection = "Data Source=.;Initial Catalog=Northwind;IntegratedSecurity=true;MultipleActiveResultSets=true";
+
+        optionsBuilder.UseSqlServer(connection);
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Category>()
+            .Property(category => category.CategoryName)
+            .IsRequired()
+            .HasMaxLength(15);
+    }
+}
