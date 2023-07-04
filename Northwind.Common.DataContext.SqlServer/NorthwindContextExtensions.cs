5699752568e66025c32fcd7dde6214177a0c5d16
Gregory Balabanov <810411@mail.ru>
2023-07-04 17:07:51 +0500

    added data context lib

---
+++ Northwind.Common.DataContext.SqlServer/NorthwindContextExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Northwind.Shared;
+
+public static class NorthwindContextExtensions
+{
+    public static IServiceCollection AddNorthwindContext(this IServiceCollection services,
+        string connectionString = "Data Source=DESKTOP-B9CIR5M\\SQLEXPRESS;Initial Catalog=Northwind;Integrated Security=true;TrustServerCertificate=True")
+    {
+        services.AddDbContext<NorthwindContext>(options => options.UseSqlServer(connectionString));
+
+        return services;
+    }
+}
