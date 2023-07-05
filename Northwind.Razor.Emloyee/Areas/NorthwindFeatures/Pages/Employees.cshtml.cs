b41ac0f7408b58ab084af8faa4eeab6f76f3cc73
Gregory Balabanov <810411@mail.ru>
2023-07-05 17:01:59 +0500

    feat: lib for employee razor page

---
+++ Northwind.Razor.Emloyee/Areas/NorthwindFeatures/Pages/Employees.cshtml.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Northwind.Shared;
+
+namespace NorthwindFeatures.Pages;
+
+public class EmpoyeesPageModel : PageModel
+{
+    private NorthwindContext db;
+
+    public EmpoyeesPageModel(NorthwindContext db)
+    {
+        this.db = db;
+    }
+
+    public Employee[] Employees { get; set; } = null!;
+
+    public void OnGet()
+    {
+        ViewData["Title"] = "Northwind B2B - Employees";
+        Employees = db.Employees
+            .OrderBy(e => e.LastName)
+            .ThenBy(e => e.FirstName)
+            .ToArray();
+    }
+}
